80b85a67f4e84e76b9f0033960174c21d1cea5a4
AkritiKapur <akritikapur1@gmail.com>
2018-11-14 19:59:46 -0700

    added project settings file

---
+++ settings.py
@@ -0,0 +1,10 @@
+"""
+    Add all project related settings here!
+"""
+
+import os
+from pathlib import Path
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATA_FOLDER = Path("{}/data/".format(PROJECT_DIR))
