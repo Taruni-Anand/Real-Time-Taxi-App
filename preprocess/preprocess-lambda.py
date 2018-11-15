6e7d119195f2de72fbbdc9aed821d34cba77c2bf
AkritiKapur <akritikapur1@gmail.com>
2018-11-14 17:41:04 -0700

    preprocess lambda initial structure added

---
+++ preprocess/preprocess-lambda.py
@@ -0,0 +1,19 @@
+"""
+    Lambda function which does the following
+    1. initial pre-processing of data
+    2. push data to s3 bucket
+"""
+
+
+def preprocess():
+    """
+        Preprocess data:
+        1. remove unnecessary columns
+        2. some calculations (maybe later)
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    preprocess()
