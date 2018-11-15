0abeb4c2a643a44720376e244e2762aabf23e551
AkritiKapur <akritikapur1@gmail.com>
2018-11-14 20:00:29 -0700

    preprocessing-removed unused columns from dataset

--- preprocess/preprocess-lambda.py
+++ preprocess/preprocess-lambda.py
@@ -3,17 +3,33 @@
     1. initial pre-processing of data
     2. push data to s3 bucket
 """
+import pandas as pd
+from settings import DATA_FOLDER
+
+DATA_FILE_PATH = DATA_FOLDER / "2014_Green_Taxi_Trip_Data.csv"
+SANITIZED_DATA_FILE_PATH = DATA_FOLDER / "sanitized_2014_Green_Taxi_Trip_Data.csv"
+
+columns_to_drop = ["Store_and_fwd_flag", "Extra", "MTA_tax", "Tolls_amount", "Ehail_fee", "Trip_type"]
 
 
-def preprocess():
+def preprocess_handler():
     """
         Preprocess data:
         1. remove unnecessary columns
         2. some calculations (maybe later)
     :return:
     """
-    pass
+    # TODO: Have this as part of S3 after discussion
+
+    # Create dataframe from csv file
+    df = pd.read_csv(DATA_FILE_PATH)
+
+    # Remove unnecessary columns
+    df.drop(columns=columns_to_drop)
+
+    # save as csv
+    df.to_csv(SANITIZED_DATA_FILE_PATH)
 
 
 if __name__ == '__main__':
-    preprocess()
+    preprocess_handler()
