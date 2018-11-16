acf9e6b4f3dba4c6b6211f34822b94a17995c8ea
AkritiKapur <akritikapur1@gmail.com>
2018-11-16 15:26:52 -0700

    changed dynamo db to mongo db

--- preprocess/structure.py
+++ preprocess/structure.py
@@ -2,29 +2,27 @@ from pyspark.conf import SparkConf
 from pyspark import SparkContext
 from pyspark.sql import SparkSession
 
-from settings import DATA_FOLDER
+from settings import DATA_FOLDER, SPARK_CONFIGURE_INPUT, DB_HOST, DB_NAME
 
 SANITIZED_DATA_FILE_PATH = DATA_FOLDER / "sanitized_2014_Green_Taxi_Trip_Data.csv"
+DB_TABLE = "taxi_info"
 
 
-def transform_data(df, spark):
+def transform_data(df):
     """
         Structures data to store in Dynamo db
     :param df: Spark dataframe to be structured
     :return:
     """
-
-    df.createOrReplaceTempView("dataframe")
-    spark.sql("INSERT OVERWRITE TABLE temp SELECT * FROM df")
     return df
 
 
-def write_df_to_dynamodb(df):
+def write_df_to_mongodb(df):
     """
         Write pyspark dataframe to dynamo db table
     :param df: pyspark df
     """
-    pass
+    df.write.format("com.mongodb.spark.sql.DefaultSource").mode("append").save()
 
 
 def read_csv(spark):
@@ -38,32 +36,23 @@ def read_csv(spark):
     return df
 
 
-def configure_dd(sc):
+def configure_spark():
     """
         Configure dynamo db
     :return:
     """
-    conf = {
-        "dynamodb.servicename": "dynamodb",
-        "dynamodb.input.tableName": "users",
-        "dynamodb.endpoint": "https://dynamodb.eu-central-1.amazonaws.com",
-        "dynamodb.regionid": "eu-central-1",
-        "mapred.output.format.class": "org.apache.hadoop.dynamodb.write.DynamoDBOutputFormat",
-        "mapred.input.format.class": "org.apache.hadoop.dynamodb.read.DynamoDBInputFormat"
-    }
+    spark = SparkSession \
+        .builder \
+        .appName("myApp") \
+        .config(SPARK_CONFIGURE_INPUT,
+                "mongodb://{}/{}.{}".format(DB_HOST, DB_NAME, DB_TABLE)) \
+        .getOrCreate()
 
-    sc.hadoopRDD(
-        inputFormatClass='org.apache.hadoop.dynamodb.read.DynamoDBInputFormat',
-        keyClass='org.apache.hadoop.io.Text',
-        valueClass='org.apache.hadoop.dynamodb.DynamoDBItemWritable',
-        conf=conf
-    )
+    return spark
 
 
 if __name__ == '__main__':
 
-    sc = SparkSession.builder.appName('dbb').getOrCreate()
-    # configure_dd(sc)
-    df = read_csv(sc)
-    # df = transform_data(df, sc)
-    write_df_to_dynamodb(df)
+    spark = configure_spark()
+    df = read_csv(spark)
+    write_df_to_mongodb(spark)
