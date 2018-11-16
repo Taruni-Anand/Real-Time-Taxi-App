df0c0b8085a338655a440dd9e45204c4577ac50d
AkritiKapur <akritikapur1@gmail.com>
2018-11-15 18:31:03 -0700

    read csv to spark dataframe added

--- preprocess/structure.py
+++ preprocess/structure.py
@@ -1,18 +1,21 @@
-from pyspark.shell import sqlContext
-from pyspark.sql import SparkSession
+from pyspark.conf import SparkConf
 from pyspark import SparkContext
+from pyspark.sql import SparkSession
 
 from settings import DATA_FOLDER
 
 SANITIZED_DATA_FILE_PATH = DATA_FOLDER / "sanitized_2014_Green_Taxi_Trip_Data.csv"
 
 
-def structure_data(df):
+def transform_data(df, spark):
     """
         Structures data to store in Dynamo db
     :param df: Spark dataframe to be structured
     :return:
     """
+
+    df.createOrReplaceTempView("dataframe")
+    spark.sql("INSERT OVERWRITE TABLE temp SELECT * FROM df")
     return df
 
 
@@ -24,20 +27,43 @@ def write_df_to_dynamodb(df):
     pass
 
 
-def read_csv():
+def read_csv(spark):
     """
         Reads sanitized csv into spark dataframe
     :return: spark dataframe
     """
 
-    df = sqlContext.read.format('csv')\
-        .options(header='true', inferSchema='true')\
-        .load(SANITIZED_DATA_FILE_PATH)
+    df = spark.read.csv(str(SANITIZED_DATA_FILE_PATH), header=True)
 
     return df
 
 
+def configure_dd(sc):
+    """
+        Configure dynamo db
+    :return:
+    """
+    conf = {
+        "dynamodb.servicename": "dynamodb",
+        "dynamodb.input.tableName": "users",
+        "dynamodb.endpoint": "https://dynamodb.eu-central-1.amazonaws.com",
+        "dynamodb.regionid": "eu-central-1",
+        "mapred.output.format.class": "org.apache.hadoop.dynamodb.write.DynamoDBOutputFormat",
+        "mapred.input.format.class": "org.apache.hadoop.dynamodb.read.DynamoDBInputFormat"
+    }
+
+    sc.hadoopRDD(
+        inputFormatClass='org.apache.hadoop.dynamodb.read.DynamoDBInputFormat',
+        keyClass='org.apache.hadoop.io.Text',
+        valueClass='org.apache.hadoop.dynamodb.DynamoDBItemWritable',
+        conf=conf
+    )
+
+
 if __name__ == '__main__':
-    df = read_csv()
-    df = structure_data(df)
+
+    sc = SparkSession.builder.appName('dbb').getOrCreate()
+    # configure_dd(sc)
+    df = read_csv(sc)
+    # df = transform_data(df, sc)
     write_df_to_dynamodb(df)
