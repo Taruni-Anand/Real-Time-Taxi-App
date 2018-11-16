4f502d61bcbc7527a231ba3920b9166bbd8b88e2
AkritiKapur <akritikapur1@gmail.com>
2018-11-16 15:27:16 -0700

    db settings added

--- settings.py
+++ settings.py
@@ -8,3 +8,9 @@ from pathlib import Path
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATA_FOLDER = Path("{}/data/".format(PROJECT_DIR))
+
+SPARK_CONFIGURE_INPUT = "spark.mongodb.input.uri"
+
+# Database setting
+DB_HOST = "localhost:27017"
+DB_NAME = "taxi"
